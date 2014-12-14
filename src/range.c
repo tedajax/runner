ff4e70281ef719c726c40daad4de1cc12e307105
Ted Dobyns <ted.ajax@gmail.com>
2014-12-13 23:49:25 -0800

    range type

---
+++ src/range.c
@@ -0,0 +1,12 @@
+#include "range.h"
+
+void range_init(Range* self, f32 min, f32 max) {
+    self->min = min;
+    self->max = max;
+}
+
+void range_load(Range* self, const char* config, const char* section, const char* key) {
+    Ini* cfg = config_get(config);
+    self->min = ini_get_float_at(cfg, section, key, 0);
+    self->max = ini_get_float_at(cfg, section, key, 1);
+}
---
+++ src/range.h
@@ -0,0 +1,22 @@
+#ifndef RUNNER_RANGE_H
+#define RUNNER_RANGE_H
+
+#include "core.h"
+
+typedef struct range_t {
+    f32 min;
+    f32 max;
+} Range;
+
+void range_init(Range* self, f32 min, f32 max);
+void range_load(Range* self, const char* config, const char* section, const char* key);
+
+inline f32 range_rand(Range* self) {
+    return randf_range(self->min, self->max);
+}
+
+inline f32 range_mid(Range* self) {
+    return (self->max + self->min) / 2.f;
+}
+
+#endif

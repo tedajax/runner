17142fa19a973b34a8f99e81df2b90bced5254e4
Ted Dobyns <ted.ajax@gmail.com>
2015-02-02 11:27:40 -0800

    remove unnecssary code in main

--- src/main.c
+++ src/main.c
@@ -1,10 +1,5 @@
 #include "application.h"
-#include "tdjxlua.h"
 
 int main(int argc, char* argv[]) {
-    //lua_State* L = lua_open();
-    //luaL_openlibs(L);
-    //luaL_dofile(L, "assets/scripts/test.lua");
-    //printf("%s\n", lua_tostring(L, -1));
     return app_run(argc, argv);
 }

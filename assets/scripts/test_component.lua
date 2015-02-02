4a0df0fc4c5e2ab6870d21d485cc765555fdf3c6
Ted Dobyns <ted.ajax@gmail.com>
2015-02-01 20:33:12 -0800

    stuff

--- assets/scripts/test_component.lua
+++ assets/scripts/test_component.lua
@@ -1,11 +1,17 @@
 require("assets/scripts/component")
 
 local timer = 1
+local counter = 0
+
+start = function(entity)
+    counter = 100
+end
 
 update = function(dt)
     timer = timer - dt
     if timer <= 0 then
-        print "alright cool"
-        timer = 1
+        print("alright cool stuff "..counter)
+        counter = counter + 10
+        timer = 0.1
     end
 end

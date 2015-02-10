692425626011babe6a5c3c7e3c8ceca2fa0bff53
Ted Dobyns <ted.ajax@gmail.com>
2015-02-10 13:31:21 -0800

    add an atlas

--- src/main.c
+++ src/main.c
@@ -1,14 +1,5 @@
-//#include "application.h"
-#include <mxml.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "application.h"
 
 int main(int argc, char* argv[]) {
-    FILE* fp;
-    mxml_node_t* tree;
-    fp = fopen("test.xml", "rb");
-    tree = mxmlLoadFile(NULL, fp, MXML_TEXT_CALLBACK);
-    fclose(fp);
-    return 0;
-    //return app_run(argc, argv);
+    return app_run(argc, argv);
 }
